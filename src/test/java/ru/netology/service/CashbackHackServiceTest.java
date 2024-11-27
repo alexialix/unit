fd10a9a26d1cfc2bcdfac6bca807243ec5eede2c
dogmou <trup.lexa@gmail.com>
2024-11-27 21:00:52 +0500

    fix test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -32,8 +32,8 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldReturn0WhenAmountIs0() {
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(0);
-        int expected = 0;  // Для 0 суммы, возвращаем 0
+        int actual = service.remain(1000);
+        int expected = 0;
         Assert.assertEquals(expected, actual);
     }
 }
